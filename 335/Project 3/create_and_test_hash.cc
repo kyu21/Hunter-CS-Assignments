05fe08bcb8c26b39057f83e1f8d1c8f68c83134c
kyu21 <kun.yu25@myhunter.cuny.edu>
2019-04-03 03:48:06 -0400

    Progress Proj 3

--- 335/Project 3/create_and_test_hash.cc
+++ 335/Project 3/create_and_test_hash.cc
@@ -1,50 +1,107 @@
-// YOUR NAME.
+// Kun Yu
+// Spring CSCI 335
+// Assignment 3
 
+#include "linear_probing.h"
 #include "quadratic_probing.h"
+#include "double_probing.h"
 #include <iostream>
 #include <fstream>
 #include <string>
 using namespace std;
 
-
 template <typename HashTableType>
-void TestFunctionForHashTable(HashTableType &hash_table, const string &words_filename, const string &query_filename) {
-  cout << "TestFunctionForHashTables..." << endl;
-  cout << "Words filename: " << words_filename << endl;
-  cout << "Query filename: " << query_filename << endl;
-  hash_table.MakeEmpty();  
-  //..Insert your own code
+void TestFunctionForHashTable(HashTableType &hash_table, const string &words_filename, const string &query_filename)
+{
+	cout << "TestFunctionForHashTables..." << endl;
+	cout << "Words filename: " << words_filename << endl;
+	cout << "Query filename: " << query_filename << endl;
+	hash_table.MakeEmpty();
 
-  cout << "Collisions: " << endl;
-  cout << "Number of items: " << endl;
-  cout << "Size of hash table: " << endl;
-  cout << "Load factor: " << endl;
-  cout << "Avg. number of collisions: " << endl;
+	hash_table.resetNumCollisions();
+
+	ifstream words_file;
+	string words_line;
+
+	words_file.open(words_filename);
+	if (words_file.fail())
+	{
+		cout << "Unable to open file\n";
+		exit(1);
+	}
+
+	while (getline(words_file, words_line))
+	{
+		hash_table.Insert(words_line);
+	}
+
+	cout << "Collisions: " << hash_table.getNumCollisions() << endl;
+	cout << "Number of items: " << hash_table.getNumElements() << endl;
+	cout << "Size of hash table: " << hash_table.getHashTableSize() << endl;
+	cout << "Load factor: " << (hash_table.getNumElements()) / ((double)hash_table.getHashTableSize()) << endl;
+	cout << "Avg. number of collisions: " << (hash_table.getNumCollisions()) / ((double)hash_table.getNumElements()) << endl;
+
+	hash_table.resetNumCollisions();
+
+	ifstream query_file;
+	string query_line;
+
+	query_file.open(query_filename);
+	if (query_file.fail())
+	{
+		cout << "Unable to open file\n";
+		exit(1);
+	}
+
+	while (getline(query_file, query_line))
+	{
+		if (hash_table.Contains(query_line))
+		{
+			cout << query_line << ": Found in " << hash_table.getNumCollisions() + 1 << " probes" << endl;
+			hash_table.resetNumCollisions();
+		}
+		else
+		{
+			cout << query_line << ": Not found in " << hash_table.getNumCollisions() + 1 << " probes" << endl;
+			hash_table.resetNumCollisions();
+		}
+	}
+
+	words_file.close();
+	query_file.close();
 }
 
 // Sample main for program create_and_test_hash
-int
-main(int argc, char **argv) {
-  if (argc != 4) {
-    cout << "Usage: " << argv[0] << " <wordsfilename> <queryfilename> <flag>" << endl;
-    return 0;
-  }
-  
-  const string words_filename(argv[1]);
-  const string query_filename(argv[2]);
-  const string param_flag(argv[3]);
+int main(int argc, char **argv)
+{
+	if (argc != 4)
+	{
+		cout << "Usage: " << argv[0] << " <wordsfilename> <queryfilename> <flag>" << endl;
+		return 0;
+	}
 
-  if (param_flag == "linear") {
-    // HashTableLinear<string> linear_probing_table;
-    // TestFunctionForHashTable(linear_probing_table, words_filename, query_filename);    
-  } else if (param_flag == "quadratic") {
-    HashTable<string> quadratic_probing_table;
-    TestFunctionForHashTable(quadratic_probing_table, words_filename, query_filename);    
-  } else if (param_flag == "double") {
-    // HashTableDouble<string> double_probing_table;
-    // TestFunctionForHashTable(double_probing_table, words_filename, query_filename);    
-  } else {
-    cout << "Uknown tree type " << param_flag << " (User should provide linear, quadratic, or double)" << endl;
-  }
-  return 0;
+	const string words_filename(argv[1]);
+	const string query_filename(argv[2]);
+	const string param_flag(argv[3]);
+
+	if (param_flag == "linear")
+	{
+		HashTableLinear<string> linear_probing_table;
+		TestFunctionForHashTable(linear_probing_table, words_filename, query_filename);
+	}
+	else if (param_flag == "quadratic")
+	{
+		HashTable<string> quadratic_probing_table;
+		TestFunctionForHashTable(quadratic_probing_table, words_filename, query_filename);
+	}
+	else if (param_flag == "double")
+	{
+		HashTableDouble<string> double_probing_table;
+		TestFunctionForHashTable(double_probing_table, words_filename, query_filename);
+	}
+	else
+	{
+		cout << "Uknown tree type " << param_flag << " (User should provide linear, quadratic, or double)" << endl;
+	}
+	return 0;
 }
--- 335/Project 3/quadratic_probing.h
+++ 335/Project 3/quadratic_probing.h
@@ -4,148 +4,165 @@
 #include <vector>
 #include <algorithm>
 #include <functional>
-
-
-namespace {
-
-// Internal method to test if a positive number is prime.
-bool IsPrime(size_t n) {
-  if( n == 2 || n == 3 )
-    return true;
-  
-  if( n == 1 || n % 2 == 0 )
-    return false;
-  
-  for( int i = 3; i * i <= n; i += 2 )
-    if( n % i == 0 )
-      return false;
-  
-  return true;
-}
-
-
-// Internal method to return a prime number at least as large as n.
-int NextPrime(size_t n) {
-  if (n % 2 == 0)
-    ++n;  
-  while (!IsPrime(n)) n += 2;  
-  return n;
-}
-
-}  // namespace
-
+#include "prime_checker.h"
 
 // Quadratic probing implementation.
 template <typename HashedObj>
-class HashTable {
- public:
-  enum EntryType {ACTIVE, EMPTY, DELETED};
+class HashTable
+{
+  public:
+	enum EntryType
+	{
+		ACTIVE,
+		EMPTY,
+		DELETED
+	};
 
-  explicit HashTable(size_t size = 101) : array_(NextPrime(size))
-    { MakeEmpty(); }
-  
-  bool Contains(const HashedObj & x) const {
-    return IsActive(FindPos(x));
-  }
-  
-  void MakeEmpty() {
-    current_size_ = 0;
-    for (auto &entry : array_)
-      entry.info_ = EMPTY;
-  }
+	explicit HashTable(size_t size = 101) : array_(NextPrime(size))
+	{
+		MakeEmpty();
+	}
 
-  bool Insert(const HashedObj & x) {
-    // Insert x as active
-    size_t current_pos = FindPos(x);
-    if (IsActive(current_pos))
-      return false;
-    
-    array_[current_pos].element_ = x;
-    array_[current_pos].info_ = ACTIVE;
-    
-    // Rehash; see Section 5.5
-    if (++current_size_ > array_.size() / 2)
-      Rehash();    
-    return true;
-  }
-    
-  bool Insert(HashedObj && x) {
-    // Insert x as active
-    size_t current_pos = FindPos(x);
-    if (IsActive(current_pos))
-      return false;
-    
-    array_[current_pos] = std::move(x);
-    array_[current_pos].info_ = ACTIVE;
+	bool Contains(const HashedObj &x) const
+	{
+		return IsActive(FindPos(x));
+	}
 
-    // Rehash; see Section 5.5
-    if (++current_size_ > array_.size() / 2)
-      Rehash();
+	void MakeEmpty()
+	{
+		current_size_ = 0;
+		for (auto &entry : array_)
+			entry.info_ = EMPTY;
+	}
 
-    return true;
-  }
+	bool Insert(const HashedObj &x)
+	{
+		// Insert x as active
+		size_t current_pos = FindPos(x);
+		if (IsActive(current_pos))
+			return false;
 
-  bool Remove(const HashedObj & x) {
-    size_t current_pos = FindPos(x);
-    if (!IsActive(current_pos))
-      return false;
+		array_[current_pos].element_ = x;
+		array_[current_pos].info_ = ACTIVE;
 
-    array_[current_pos].info_ = DELETED;
-    return true;
-  }
+		// Rehash; see Section 5.5
+		if (++current_size_ > array_.size() / 2)
+			Rehash();
+		return true;
+	}
 
- private:        
-  struct HashEntry {
-    HashedObj element_;
-    EntryType info_;
-    
-    HashEntry(const HashedObj& e = HashedObj{}, EntryType i = EMPTY)
-    :element_{e}, info_{i} { }
-    
-    HashEntry(HashedObj && e, EntryType i = EMPTY)
-    :element_{std::move(e)}, info_{ i } {}
-  };
-    
+	bool Insert(HashedObj &&x)
+	{
+		// Insert x as active
+		size_t current_pos = FindPos(x);
+		if (IsActive(current_pos))
+			return false;
 
-  std::vector<HashEntry> array_;
-  size_t current_size_;
+		array_[current_pos] = std::move(x);
+		array_[current_pos].info_ = ACTIVE;
 
-  bool IsActive(size_t current_pos) const
-  { return array_[current_pos].info_ == ACTIVE; }
+		// Rehash; see Section 5.5
+		if (++current_size_ > array_.size() / 2)
+			Rehash();
 
-  size_t FindPos(const HashedObj & x) const {
-    size_t offset = 1;
-    size_t current_pos = InternalHash(x);
-      
-    while (array_[current_pos].info_ != EMPTY &&
-	   array_[current_pos].element_ != x) {
-      current_pos += offset;  // Compute ith probe.
-      offset += 2;
-      if (current_pos >= array_.size())
-	current_pos -= array_.size();
-    }
-    return current_pos;
-  }
+		return true;
+	}
 
-  void Rehash() {
-    std::vector<HashEntry> old_array = array_;
+	bool Remove(const HashedObj &x)
+	{
+		size_t current_pos = FindPos(x);
+		if (!IsActive(current_pos))
+			return false;
 
-    // Create new double-sized, empty table.
-    array_.resize(NextPrime(2 * old_array.size()));
-    for (auto & entry : array_)
-      entry.info_ = EMPTY;
-    
-    // Copy table over.
-    current_size_ = 0;
-    for (auto & entry :old_array)
-      if (entry.info_ == ACTIVE)
-	Insert(std::move(entry.element_));
-  }
-  
-  size_t InternalHash(const HashedObj & x) const {
-    static std::hash<HashedObj> hf;
-    return hf(x) % array_.size( );
-  }
+		array_[current_pos].info_ = DELETED;
+		return true;
+	}
+
+	// Custom Funcitons to Count Collisions
+	int getNumElements()
+	{
+		return current_size_;
+	}
+
+	void resetNumCollisions()
+	{
+		num_collisions_ = 0;
+	}
+
+	int getNumCollisions()
+	{
+		return num_collisions_;
+	}
+
+	int getHashTableSize()
+	{
+		return array_.size();
+	}
+
+  private:
+	struct HashEntry
+	{
+		HashedObj element_;
+		EntryType info_;
+
+		HashEntry(const HashedObj &e = HashedObj{}, EntryType i = EMPTY)
+			: element_{e}, info_{i} {}
+
+		HashEntry(HashedObj &&e, EntryType i = EMPTY)
+			: element_{std::move(e)}, info_{i} {}
+	};
+
+	std::vector<HashEntry> array_;
+	size_t current_size_;
+
+	// Number of Collisions
+	mutable size_t num_collisions_;
+
+	bool IsActive(size_t current_pos) const
+	{
+		return array_[current_pos].info_ == ACTIVE;
+	}
+
+	size_t FindPos(const HashedObj &x) const
+	{
+		size_t offset = 1;
+		size_t current_pos = InternalHash(x);
+
+		while (array_[current_pos].info_ != EMPTY &&
+			   array_[current_pos].element_ != x)
+		{
+
+			num_collisions_++;
+
+			current_pos += offset; // Compute ith probe.
+			offset += 2;
+			if (current_pos >= array_.size())
+				current_pos -= array_.size();
+		}
+		return current_pos;
+	}
+
+	void Rehash()
+	{
+		std::vector<HashEntry> old_array = array_;
+
+		// Create new double-sized, empty table.
+		array_.resize(NextPrime(2 * old_array.size()));
+		for (auto &entry : array_)
+			entry.info_ = EMPTY;
+
+		// Copy table over.
+		current_size_ = 0;
+		for (auto &entry : old_array)
+			if (entry.info_ == ACTIVE)
+				Insert(std::move(entry.element_));
+	}
+
+	size_t InternalHash(const HashedObj &x) const
+	{
+		static std::hash<HashedObj> hf;
+		return hf(x) % array_.size();
+	}
 };
 
-#endif  // QUADRATIC_PROBING_H
+#endif // QUADRATIC_PROBING_H
